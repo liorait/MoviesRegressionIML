7d76bc26ced408b2076805ab9f3a1484120e8577
hodaya levinson <hodaya.levinson@huji.ac.il>
2021-06-03 12:22:52 +0300

    write fit, predicd and function that help us decide the degree of the polynomial

--- task1/preprocessing.py
+++ task1/preprocessing.py
@@ -1,44 +1,31 @@
-import copy
-cast_dict={}
-keyword_dict={}
-crew_dict={}
-DEPARTMENT="known_for_department"
+cast_dict = {}
+keyword_dict = {}
+crew_dict = {}
+KNOWN_FOR = ""
 
-PRODUCER = "Production"
-DIRECTOR = "Directing"
-WRITER = "Writing"
 
 # some change
 
-def add_value_to_cast(cast_data, revenue):
+def add_value_to_cast(cast_data, box_office):
     for actor in cast_data:
-        actor_id_num=actor[id]
+        actor_id_num = actor[id]
         if actor_id_num in cast_dict:
-            cast_dict[actor_id_num].append(revenue)
+            cast_dict[actor_id_num].append(box_office)
         else:
-            cast_dict[actor_id_num]=[revenue];
-
-def add_value_to_crew(crew_data, revenue):
-    for worker in crew_data:
-        if worker[DEPARTMENT] in [PRODUCER,DIRECTOR,WRITER]:
-            actor_id_num=worker[id]
-            if actor_id_num in crew_dict:
-                crew_dict[actor_id_num].append(revenue)
-            else:
-                crew_dict[actor_id_num]=[revenue]
+            cast_dict[actor_id_num] = [box_office];
 
 
-def calculate_crew_value():
-    for key in crew_dict:
-        list= crew_dict[key]
-        new_val = sum[list]/len(list)
-        crew_dict[key]=new_val
+def add_value_to_crew(crew_data, box_office):
+    for actor in crew_data:
+        actor_id_num = actor[id]
+        if actor_id_num in crew_dict:
+            crew_dict[actor_id_num].append(box_office)
+        else:
+            crew_dict[actor_id_num] = [box_office]
+
 
 def calculate_crew_value():
     for key in crew_dict:
-        list= crew_dict[key]
-        new_val = sum[list]/len(list)
-        crew_dict[key]=new_val
-
-def process_begin(data):
-    processed_data = copy.deepcopy (data)
+        list = crew_dict[key]
+        new_val = sum[list] / len(list)
+        crew_dict[key] = new_val
