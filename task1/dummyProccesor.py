75ade784796fd188cf16a3d7e17a8f985f1abbed
hagarberick <hagar.berick@mail.huji.ac.il>
2021-06-03 12:44:45 +0300

    finish original languge and some more little features

--- task1/dummyProccesor.py
+++ task1/dummyProccesor.py
@@ -14,16 +14,32 @@ def add_dummy(data, feature):
 
 
 def belongs_to_collection(data):
-    print(data["belongs_to_collection"])
+    data['belongs_to_collection'] = data['belongs_to_collection'].fillna(0)
+    data.loc[data.belongs_to_collection != 0, 'belongs_to_collection'] = 1
 
 
 def do_changes(data):
     remove_features = ["id", "homepage", "original_title"]
     for feature in remove_features:
-        data = remove(feature)
-    belongs_to_collection()
+        data = remove(data, feature)
+    belongs_to_collection(data)
+
+
+def genre(data):
+    pass
+
+
+def original_language(data):
+    most_common = ['en', 'fr', 'hi', 'ru', 'es', 'ja']
+    data.loc[-data.original_language.isin(most_common), 'original_language'] = 'other'
+    dummy = pd.get_dummies(data.original_language, prefix='original_language')
+    data = pd.concat([data, dummy], axis=1)
+    data = remove(data, 'original_language')
+    return data
 
 
 if __name__ == '__main__':
+    # behatzlacha kapara
     movies_df = pd.read_csv("movies_dataset.csv")
-    belongs_to_collection(movies_df)
+    genre(movies_df)
+    original_language(movies_df)
