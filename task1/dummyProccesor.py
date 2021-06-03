9fb4e3ccf208cc693d51af1394d7a42801d0019e
hagarberick <hagar.berick@mail.huji.ac.il>
2021-06-03 15:07:05 +0300

    feature processing from id to production_countries

--- task1/dummyProccesor.py
+++ task1/dummyProccesor.py
@@ -1,3 +1,4 @@
+import ast
 import numpy as np
 import pandas as pd
 
@@ -16,17 +17,33 @@ def add_dummy(data, feature):
 def belongs_to_collection(data):
     data['belongs_to_collection'] = data['belongs_to_collection'].fillna(0)
     data.loc[data.belongs_to_collection != 0, 'belongs_to_collection'] = 1
+    return data
 
 
 def do_changes(data):
-    remove_features = ["id", "homepage", "original_title"]
+    remove_features = ["id", "homepage", "original_title",
+                       "production_companies", "production_countries"]
     for feature in remove_features:
         data = remove(data, feature)
-    belongs_to_collection(data)
+    data = belongs_to_collection(data)
+    data = original_language(data)
+    data = genre(data)
 
 
 def genre(data):
-    pass
+    genre_load = data["genres"].apply(ast.literal_eval)
+    for i in range(len(genre_load)):
+        genre_list = genre_load[i]
+        arr = []
+        for genre_dict in genre_list:
+            if 'name' in genre_dict:
+                arr.append(genre_dict['name'])
+        genre_load[i] = arr
+    data['genres'] = genre_load
+    dummy = pd.get_dummies(data.genres.apply(pd.Series).stack()).sum(level=0)
+    data = pd.concat([data, dummy], axis=1)
+    data = remove(data, 'genres')
+    return data
 
 
 def original_language(data):
@@ -38,8 +55,55 @@ def original_language(data):
     return data
 
 
+def production_companies(data):
+    pc_load = data["production_companies"].apply(ast.literal_eval)
+    for i in range(len(pc_load)):
+        pc_list = pc_load[i]
+        arr = []
+        for pc_dict in pc_list:
+            if 'name' in pc_dict:
+                arr.append(pc_dict['name'])
+        pc_load[i] = arr
+    data['production_companies'] = pc_load
+
+    all = []
+    for i in range(len(pc_load)):
+        all += pc_load[i]
+    all = np.array(all)
+    print(len(np.unique(all)))
+    print(np.unique(all))
+
+    # dummy = pd.get_dummies(data.genres.apply(pd.Series).stack()).sum(level=0)
+    # data = pd.concat([data, dummy], axis=1)
+    # data = remove(data, 'production_companies')
+    return data
+
+
+def production_countries(data):
+    pc_load = data["production_countries"].apply(ast.literal_eval)
+    for i in range(len(pc_load)):
+        pc_list = pc_load[i]
+        arr = []
+        for pc_dict in pc_list:
+            if 'name' in pc_dict:
+                arr.append(pc_dict['name'])
+        pc_load[i] = arr
+    data['production_countries'] = pc_load
+
+    all = []
+    for i in range(len(pc_load)):
+        all += pc_load[i]
+    all = np.array(all)
+    print(len(np.unique(all)))
+    print(np.unique(all))
+
+    # dummy = pd.get_dummies(data.genres.apply(pd.Series).stack()).sum(level=0)
+    # data = pd.concat([data, dummy], axis=1)
+    # data = remove(data, 'production_companies')
+    return data
+
+
 if __name__ == '__main__':
     # behatzlacha kapara
     movies_df = pd.read_csv("movies_dataset.csv")
-    genre(movies_df)
-    original_language(movies_df)
+    production_countries(movies_df)
